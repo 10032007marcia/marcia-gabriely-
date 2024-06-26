de09f24ee015d356e42b94490d750f4c3c21e30f
10032007marcia <ferreira.ricardo.marcia@escola.pr.gov.br>
2024-06-26 07:54:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const caixaprincipal = document.querySelector(".caixa-principal");
+const caixaperguntas = document.querySelector(".caixa-perguntas");
+const caixaalternativas = document.querySelector(".caixa-alternativsa");
+const caixaresultados = document.querySelector(".caixa-resultados");
+const caixareultados = document.querySelector(".caixa-resultado");
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+];
